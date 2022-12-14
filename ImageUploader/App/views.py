f10f9a966f6d7aa90c5ad26c6fe115167e86f11e
Harshil Khatri <khatriharshil26@gmail.com>
2022-12-14 22:00:49 +0530

    Build Image Uploader Application

---
+++ ImageUploader/App/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import ImageForm
+from django.http import HttpResponseRedirect
+from .models import Image
+
+# This function normally use to upload image
+def home(request):
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/")
+    else:
+        form = ImageForm()
+    images = Image.objects.all()
+    return render(request, "home.html", {"form": form, "images": images})

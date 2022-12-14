f34ac7644bf075d38d4e78f36e15448e9335f9f1
Harshil Khatri <khatriharshil26@gmail.com>
2022-12-14 22:20:01 +0530

    Build Image Uploader Application

--- ImageUploader/App/views.py
+++ ImageUploader/App/views.py
@@ -6,6 +6,7 @@ from .models import Image
 # This function normally use to upload image
 def home(request):
     if request.method == "POST":
+        print(request.FILES["photo"])
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
